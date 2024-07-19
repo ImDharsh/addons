106e4c81c16ab3a131e7d4cbf0365175de27ed45
tarcinrobotics <devops@tarcinacademy.in>
2024-07-19 20:07:54 +0530

    made some changes

---
+++ student_port_ajmeer/__manifest__.py
@@ -0,0 +1,19 @@
+# student/__manifest__.py
+{
+    'name': 'Student Portal Ajmeer',
+    'version': '1.1',
+    'summary': 'Ajmeer is working on this Students Portal View inside portal',
+    'description': 'A module to manage student records.',
+    'author': 'Tarcin Inc',
+    'category': 'Portal',
+    'depends': ['base', 'portal','web','website'],
+    'data': [
+      #  'security/ir.model.access.csv',
+#        'views/student_action_views.xml',
+        'views/student_views.xml',
+        'views/student_ajmeer.xml',
+#        'views/student_education.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
